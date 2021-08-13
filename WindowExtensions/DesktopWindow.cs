c6a88f6d08e732df7ae1dd1334757bd83e5ddf8b
josetr <37419832+josetr@users.noreply.github.com>
2021-08-13 21:57:43 +0100

    Load window icon with correct recommend sizes to prevent upscaling / downscaling

--- WindowExtensions/DesktopWindow.cs
+++ WindowExtensions/DesktopWindow.cs
@@ -290,12 +290,28 @@ namespace WinUIExtensions.Desktop
 
         private void LoadIcon(IntPtr hwnd, string iconName)
         {
+            const int ICON_SMALL = 0;
+            const int ICON_BIG = 1;
 
-            IntPtr hIcon = PInvoke.User32.LoadImage(IntPtr.Zero, iconName,
-                PInvoke.User32.ImageType.IMAGE_ICON, 16, 16, PInvoke.User32.LoadImageFlags.LR_LOADFROMFILE);
+            var hSmallIcon = PInvoke.User32.LoadImage(
+                IntPtr.Zero,
+                iconName,
+                PInvoke.User32.ImageType.IMAGE_ICON,
+                PInvoke.User32.GetSystemMetrics(PInvoke.User32.SystemMetric.SM_CXSMICON),
+                PInvoke.User32.GetSystemMetrics(PInvoke.User32.SystemMetric.SM_CYSMICON),
+                PInvoke.User32.LoadImageFlags.LR_LOADFROMFILE);
 
-            PInvoke.User32.SendMessage(hwnd, PInvoke.User32.WindowMessage.WM_SETICON, (IntPtr)0, hIcon);
+            PInvoke.User32.SendMessage(hwnd, PInvoke.User32.WindowMessage.WM_SETICON, (IntPtr)ICON_SMALL, hSmallIcon);
 
+            var hBigIcon = PInvoke.User32.LoadImage(
+                IntPtr.Zero,
+                iconName,
+                PInvoke.User32.ImageType.IMAGE_ICON,
+                PInvoke.User32.GetSystemMetrics(PInvoke.User32.SystemMetric.SM_CXICON),
+                PInvoke.User32.GetSystemMetrics(PInvoke.User32.SystemMetric.SM_CYICON),
+                PInvoke.User32.LoadImageFlags.LR_LOADFROMFILE);
+
+            PInvoke.User32.SendMessage(hwnd, PInvoke.User32.WindowMessage.WM_SETICON, (IntPtr)ICON_BIG, hBigIcon);
         }
 
         [StructLayout(LayoutKind.Sequential)]
